85cd09f42b265370b780a8a37e27c25d664aed0c
s0wie <sowieh@gmail.com>
2021-10-26 18:50:06 +0200

    Create data.php
    First commit, adding data

---
+++ data.php
@@ -0,0 +1,112 @@
+<?php
+
+declare(strict_types=1);
+
+$slideshowImages =
+  [
+    [
+      'img' => '/../images/announcement.png',
+      'text' => 'Before the first game',
+    ],
+    [
+      'img' => '/../images/cookie-game.png',
+      'text' => 'Scene from the Honey Cookie game',
+    ],
+    [
+      'img' => '/../images/stairmaze.jpeg',
+      'text' => 'The stairmaze inspired by the famous artwork',
+    ],
+  ];
+
+
+$characters =
+  [
+    [
+      'name' => 'Seong Gi-hun',
+      'player-tag' => 456,
+      'actor' => 'Lee Jung-jae',
+      'actor-img' => '/../images/player456.png',
+      'character-info' => 'Protagonist, a divorced chauffeur who lives with his mother and struggles with a gambling addiction which gets in the way of his relationship with his daughter.',
+      'traits' =>
+      [
+        'caring', 'kind', 'reckless',
+      ]
+    ],
+    [
+      'name' => 'Cho Sang-woo',
+      'player-tag' => 218,
+      'actor' => 'Park Hae-soo',
+      'actor-img' => '/../images/player218.png',
+      'character-info' => 'A childhood friend of Seong Gi-hun, who became a successful businessman, but involved himself in illegal activities which now makes him wanted by the law.',
+      'traits' =>
+      [
+        'strategic', 'intelligent', 'cunning', 'selfish',
+      ]
+    ],
+    [
+      'name' => 'Kan Sae-byeok',
+      'player-tag' => 067,
+      'actor' => 'Jung Ho-yeon',
+      'actor-img' => '/../images/player067.png',
+      'character-info' => 'A survivor from North Korea, struggling to take care of her brother and keeping her promise to find money in order to bring her mother to South Korea.',
+      'traits' =>
+      [
+        'smart', 'calm', 'secretive',
+      ],
+    ],
+    [
+      'name' => 'Oh Il-nam',
+      'player-tag' => 001,
+      'actor' => 'Oh Yeong-su',
+      'actor-img' => '/../images/player001.png',
+      'character-info' => 'An elderly man dying of a brain tumor and joined the games to relive the joy of being alive.',
+      'traits' =>
+      [
+        'happy', 'playful', 'smart', 'careless',
+      ],
+    ],
+    [
+      'name' => 'Ali Abdul',
+      'player-tag' => 199,
+      'actor' => 'Anupam Tripathi',
+      'actor-img' => '/../images/player199.png',
+      'character-info' => 'A pakistani immigrant struggling to provide for his wife and infant.',
+      'traits' =>
+      [
+        'kind', 'team-player', 'naive',
+      ],
+    ],
+    [
+      'name' => 'Jang Deok-su',
+      'player-tag' => 101,
+      'actor' => 'Heo Sung-tae',
+      'actor-img' => '/../images/player101.png',
+      'character-info' => 'A gangster trapped in gaambling debts to fellow gangsters',
+      'traits' =>
+      [
+        'strong', 'bully', 'violent'
+      ],
+    ],
+    [
+      'name' => 'Han Mi-nyeo',
+      'player-tag' => 212,
+      'actor' => 'Kim Joo-ryuong',
+      'actor-img' => '/../images/player212.png',
+      'character-info' => 'A fraudster who claims to be a poor single mother',
+      'traits' =>
+      [
+        'bold', 'loud', 'manipulative'
+      ]
+    ],
+    [
+      'name' => 'Hwang Jun-ho',
+      'player-tag' => null,
+      'actor' => 'Wi Ha-joon',
+      'actor-img' => '/../images/policeofficer.png',
+      'character-info' => 'A police officer who sneaks into the games to find his missing brother.',
+      'traits' =>
+      [
+        'fair', 'brave', 'sneaky'
+      ]
+    ]
+  ];
